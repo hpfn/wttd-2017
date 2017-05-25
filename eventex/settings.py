f10f2671d987ec4aa509ffe313b7183b4a718dbe
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-05-25 11:26:04 -0300

    static files from AWS S3

--- eventex/settings.py
+++ eventex/settings.py
@@ -100,5 +100,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+STATIC_URL = 'http://s3.amazonaws.com/wttd-eventex-hpfn/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
