993888a587535c28eff01a1c644502bf063915d4
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-06-17 07:28:26 -0300

    try/except is necessary. even using validators and django stuff.

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -2,12 +2,21 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 
+
 def validate_cpf(value):
     if not value.isdigit():
         raise ValidationError('CPF deve conter apenas números.', 'digits')
 
     if len(value) != 11:
         raise ValidationError('CPF deve ter 11 números.', 'length')
+
+#def validate_email(value):
+#    from django.core.validators import validate_email
+#    try:
+#        validate_email(value)
+#    except ValidationError:
+#        raise ValidationError('E-mail inválido', 'invalid email')
+
 
 
 class SubscriptionForm(forms.Form):
